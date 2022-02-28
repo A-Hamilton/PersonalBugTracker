55d60b94554ff2b831417b72a7821fd095f60283
Adam <63732042+A-Hamilton@users.noreply.github.com>
2022-02-28 11:58:20 +0000

    Mongodb implementation code

--- PersonalBugTracker/ViewModel/MainViewModel.cs
+++ PersonalBugTracker/ViewModel/MainViewModel.cs
@@ -8,6 +8,18 @@ namespace PersonalBugTracker.ViewModel
 {
     class MainViewModel
     {
+        protected virtual void OnExit (System.Windows.ExitEventArgs e){
+            saveToDatabase();
+        }
+        
+        private void saveToDatabase (){
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Retoggle:<password>@cluster0.d2mcw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("db");
+            var collection = database.GetCollection("BT");
+            collection.InsertOne("data");
+        }
 
     }
 }
